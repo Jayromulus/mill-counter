e67c945b7323092a9132f9964d1008c912b2a53b
Jayromulus <jerbear2@gmail.com>
2026-05-28 21:37:25 -0400

    testing on phone

--- main.js
+++ main.js
@@ -8,6 +8,9 @@ const squall = document.getElementById('sorcerous-squall');
 const stream = document.getElementById('stream-of-thought');
 const surgical = document.getElementById('surgical-extraction');
 const crab = document.getElementById('ruin-crab');
+const draw = document.getElementById('draw');
+const discardCard = document.getElementById('discard-card');
+const cardOnTop = document.getElementById('card-on-top');
 
 const deck = document.getElementById('deck-count');
 const discard = document.getElementById('discard-count');
@@ -35,10 +38,14 @@ const pageElements = [custom, jace, thought, tidecaller, secrets, archive, squal
 
 let deckSize = 53;
 let discardSize = 0;
+let runningTotal = [];
+let currentModifier = 'subtract';
 
 function renderPage() {
   deck.innerText = deckSize;
   discard.innerText = discardSize;
+  current.innerText = runningTotal.join('');
+  modifier.innerText = currentModifier === 'add' ? '+' : '-';
 }
 
 function mill(amount) {
@@ -59,17 +66,69 @@ function togglePopup(open) {
   open ? customPopup.classList.remove('hidden') : customPopup.classList.add('hidden');
 }
 
+function addNumber(num) {
+  runningTotal.push(num);
+  
+  renderPage();
+}
+
+function removeNumber() {
+  runningTotal.pop();
+
+  renderPage();
+}
+
+function toggleModifier() {
+  currentModifier = currentModifier === 'subtract' ? 'add' : 'subtract'
+
+  renderPage();
+}
+
+function submitCustom() {
+  if (runningTotal.length > 0) mill(parseInt(runningTotal.join('')));
+  togglePopup(false);
+  while (runningTotal.length > 0) runningTotal.pop();
+  renderPage();
+}
+
+function deckManip(amount, deck, discard) {
+  if (deck) deckSize -= amount;
+  if (discard) discardSize += amount;
+
+  renderPage();
+}
+
 custom.addEventListener('click', e => togglePopup(true));
 jace.addEventListener('click', e => togglePopup(true));
+surgical.addEventListener('click', e => togglePopup(true));
+
+draw.addEventListener('click', e => deckManip(1, true, false))
+discardCard.addEventListener('click', e => deckManip(1, false, true))
+cardOnTop.addEventListener('click', e => deckManip(-1, true, false))
+
 thought.addEventListener('click', e => mill(2));
 tidecaller.addEventListener('click', e => mill(3));
 secrets.addEventListener('click', e => mill(2));
 archive.addEventListener('click', e => mill(13));
 squall.addEventListener('click', e => mill(9));
 stream.addEventListener('click', e => mill(4));
-surgical.addEventListener('click', e => togglePopup(true));
 crab.addEventListener('click', e => mill(3));
+
+
+one.addEventListener('click', e => addNumber(1));
+two.addEventListener('click', e => addNumber(2));
+three.addEventListener('click', e => addNumber(3));
+four.addEventListener('click', e => addNumber(4));
+five.addEventListener('click', e => addNumber(5));
+six.addEventListener('click', e => addNumber(6));
+seven.addEventListener('click', e => addNumber(7));
+eight.addEventListener('click', e => addNumber(8));
+nine.addEventListener('click', e => addNumber(9));
+zero.addEventListener('click', e => addNumber(0));
+
+modifier.addEventListener('click', e => toggleModifier());
+backspace.addEventListener('click', e => removeNumber());
 
 pageElements.forEach(el => el.classList.add('blurred'));
 reset.addEventListener('click', e => resetGame());
-submit.addEventListener('click', e => togglePopup(false));
+submit.addEventListener('click', e => submitCustom());
