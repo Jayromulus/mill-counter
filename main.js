bf7b68fc4f396a05719306ce58aedecdb3a0309d
Jayromulus <jerbear2@gmail.com>
2026-05-28 20:58:37 -0400

    testing mobile popup

--- main.js
+++ main.js
@@ -14,6 +14,23 @@ const discard = document.getElementById('discard-count');
 
 const reset = document.getElementById('reset-game');
 
+const customPopup = document.getElementById('custom-input');
+
+const one = document.getElementById('one');
+const two = document.getElementById('two');
+const three = document.getElementById('three');
+const four = document.getElementById('four');
+const five = document.getElementById('five');
+const six = document.getElementById('six');
+const seven = document.getElementById('seven');
+const eight = document.getElementById('eight');
+const nine = document.getElementById('nine');
+const modifier = document.getElementById('modifier');
+const zero = document.getElementById('zero');
+const backspace = document.getElementById('backspace');
+const current = document.getElementById('current');
+const submit = document.getElementById('submit');
+
 const pageElements = [custom, jace, thought, tidecaller, secrets, archive, squall, stream, surgical, crab];
 
 let deckSize = 53;
@@ -38,16 +55,21 @@ function resetGame() {
   renderPage();
 }
 
-custom.addEventListener('click', e => mill(-2));
-jace.addEventListener('click', e => mill(-3));
+function togglePopup(open) {
+  open ? customPopup.classList.remove('hidden') : customPopup.classList.add('hidden');
+}
+
+custom.addEventListener('click', e => togglePopup(true));
+jace.addEventListener('click', e => togglePopup(true));
 thought.addEventListener('click', e => mill(2));
 tidecaller.addEventListener('click', e => mill(3));
 secrets.addEventListener('click', e => mill(2));
 archive.addEventListener('click', e => mill(13));
 squall.addEventListener('click', e => mill(9));
 stream.addEventListener('click', e => mill(4));
-surgical.addEventListener('click', e => mill(-1));
+surgical.addEventListener('click', e => togglePopup(true));
 crab.addEventListener('click', e => mill(3));
 
 pageElements.forEach(el => el.classList.add('blurred'));
 reset.addEventListener('click', e => resetGame());
+submit.addEventListener('click', e => togglePopup(false));
